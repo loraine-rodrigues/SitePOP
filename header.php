1390d318170c2b4cc0aaa2ae2f2c4c52e53f03dd
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-20 02:49:39 -0300

    Comentários atualizados

--- header.php
+++ header.php
@@ -9,23 +9,25 @@ echo '<!doctype html>
     <meta http-equiv="Content-Language" content="pt-br">
     ';
 
-echo "<title>POP! - $title</title>"; //
+echo "<title>POP! - $title</title>"; //Para receber o titulo
 
 echo '
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" href="terceiros/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="terceiros/bootstrap/css/sticky-footer.css">
-    <script src="terceiros/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> <!-- llink importado para font Montserrat -->
+    <link rel="stylesheet" href="/terceiros/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/terceiros/bootstrap/css/sticky-footer.css">
+    <script src="/terceiros/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> <!-- link importado para font Montserrat -->
+    
+    <!--Style para padronização de todas as páginas -->
     <style>
           body {              
             background-color: #009999;                      // cor de fundo da pagina 
             font-family: \'Montserrat\', sans-serif;        // fonte do site
          }
          .navbar-customizada{
-            background-color:#6c6d6d;
+            background-color:#6c6d6d;                       //COR DA NAVBAR
          }
          .card {
             border-radius: 25px;
@@ -33,13 +35,16 @@ echo '
     </style>
 </head>
 <body class="d-flex flex-column h-100">
-    <header>           <!--INICIO da navbar-->
+    <header>    
+        
+       <!--INICIO da navbar-->
         <nav class="navbar navbar-expand-lg navbar-customizada navbar-dark"> 
-          <a class="navbar-brand" href="index.php">POP!</a>
+          <a class="navbar-brand" href="/index.php">POP!</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-                        <!--Iténs da navbar -->
+          
+          <!--Iténs da navbar -->
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item mx-2">
@@ -60,11 +65,21 @@ echo '
               <li class="nav-item mx-2">
                 <a class="nav-link" href="/contato.php">Contato</a>
               </li>
-              
-               
             </ul>
+            
+            <!--BOTÃO SAIR-->                                    
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <div class="btn-nav">
+                        <a class="btn btn-warning btn-smoll navbar-btn"> Sair </a>                   
+                    </div>
+                </li>                                
+            </ul>
+            
           </div>
-        </nav>      <!--FIM da navbar-->
+        </nav> 
+        <!--FIM da navbar-->    
+            
     </header>
     <main role="main" class="flex-shrink-0">
     ';
--- index.php
+++ index.php
@@ -5,22 +5,30 @@
 
         <div class="container text-center">
             <h1 class="display-1 font-weight-normal mb-n2" >POP!</h1>
-            <h5 class="ml-4">Liberdade para negóciar.</h5>
+            <h5 class="ml-4">Liberdade para negociar.</h5>
             <br>
-            <div class="card m-auto text-left" style="width: 24rem;"> <!--Div usada para formartar o card de login -->
+            <div class="card m-auto text-left" style="width: 24rem;">
+                <!--Div usada para formartar o card de login -->
                 <div class="card-body">
                     <form id="test">
+
+                        <!--Entrada de email para login-->
                         <div class="form-group">
-                            <label for="email"> Email: </label> <!--Entrada de email para login-->
+                            <label for="email"> Email: </label>
                             <input type="text" class="form-control" id="email" placeholder="Digite seu email">
                         </div>
 
+                        <!--Entrada de senha para login-->
                         <div class="form-group">
-                            <label for="senha"> Senha: </label> <!--Entrada de senha para login-->
+                            <label for="senha"> Senha: </label>
                             <input type="password" class="form-control" id="senha" placeholder="Digite sua senha">
                         </div>
-                        <button type="submit" class="btn btn-outline-primary btn-block">Entrar </button> <!--Botão entrar-->
+
+                        <!-- Botão entrar -->
+                        <button type="submit" class="btn btn-outline-primary btn-block">Entrar </button>
                     </form>
+
+                    <!-- Esqueci a senha / Cadastre-se -->
                     <div class="text-right mt-3">
                         <span class="small"><a href="#">Esqueci minha senha</a> </span>
                         <span class="small ml-4 mr-2"><a href="#" data-toggle="modal" data-target="#modal">Cadastre-se</a> </span>
@@ -31,19 +39,18 @@
         </div>
 
         <!--Modal para cadastro de CLIENTE ou MOTOFRETISTA-->
-
         <div id="modal" class="modal fade" role="dialog">
             <div class="modal-dialog">
 
-                <!-- Modal content-->
+                <!-- Conteúdo do modal -->
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Para iniciar o cadastro selecione:</h5>
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
                     <div class="modal-body">
-                        <a href="cadastroMotofretista.php" class="btn btn-outline-warning">Sou motofretista</a>
-                        <a href="cadastroMotofretista.php" class="btn btn-outline-info float-right">Sou cliente</a>
+                        <a href="Cadastro/motofretista.php" class="btn btn-outline-warning">Sou motofretista</a>
+                        <a href="Cadastro/cliente.php" class="btn btn-outline-info float-right">Sou cliente</a>
                     </div>
 
                 </div>
