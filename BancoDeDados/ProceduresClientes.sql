4aa8b5547445929d5e13e04f8874924517756705
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-26 17:07:36 -0300

    Criação da procedure cliente

--- BancoDeDados/ProceduresClientes.sql
+++ BancoDeDados/ProceduresClientes.sql
@@ -1,14 +1,55 @@
+-- PROCEDURES DE CLIENTE
+
 USE `motofrete`;
+
+
+-- INSERIR CLIENTE 
 DROP procedure IF EXISTS `cadastrarCliente`;
 
 DELIMITER $$
 USE `motofrete`$$
 CREATE PROCEDURE `cadastrarCliente` (IN nome varchar(255), IN nascimento date, IN cpf char(11), IN email varchar(100), IN celular varchar(13), IN senha varchar(45))
 BEGIN
-	INSERT INTO tb_cliente (nm_cliente, dt_nasc, id_cpf, nm_email, cd_celular)
+	INSERT INTO tb_cliente (nm_cliente, dt_nascimento, id_cpf, nm_email, cd_celular)
 		VALUES (nome, nascimento, cpf, email, celular);
 	INSERT INTO tb_login (nm_usuario, id_senha, id_tipo_login)
 		VALUES (email, md5(senha), '1');
 END$$
 
+DELIMITER ;
+
+
+-- EDITAR MOTOFRETISTA
+drop procedure if exists `editarCliente`;
+
+DELIMITER $$ 
+create procedure editarCliente (in id int(11), IN nome varchar(255), IN nascimento date, IN cpf char(11), IN email varchar(100), IN celular varchar(13))
+
+begin
+	update tb_cliente set nm_cliente = nome, dt_nascimento = nascimento, id_cpf = cpf, nm_email = email, cd_celular = celular where id_cliente = id;
+end $$
+DELIMITER ;
+
+
+-- BUSCAR CLIENTE
+drop procedure if exists `buscarCliente`;
+
+DELIMITER $$
+create procedure buscarCliente (in id int(11))
+
+begin
+	select * from tb_cliente where id_cliente = id;
+end $$
+DELIMITER ; 
+
+
+-- DELETAR CLIENTE  
+drop procedure if exists deletarCliente;
+
+DELIMITER $$
+create procedure deletarCliente(in id int)
+
+begin
+	delete from tb_cliente where id_cliente = id;
+end $$
 DELIMITER ;
