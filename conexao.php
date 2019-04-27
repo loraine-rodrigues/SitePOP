738acd5b549809f061ca0508cdaf5c30e79f3eb3
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-27 00:17:56 -0300

    atualização

--- conexao.php
+++ conexao.php
@@ -9,9 +9,7 @@ $porta = "50526";
 try {
     $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $banco, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
+} 
 catch (PDOException $excecao) {
     echo "Erro: $excecao->errorInfo";
 }
-
-$conexao->set_charset('utf8');
