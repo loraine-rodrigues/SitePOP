7a0a0bae0c5e9fc2d85bf416c7adc4811972bda4
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-27 00:38:33 -0300

    Correção de conexão

--- conexao.php
+++ conexao.php
@@ -7,9 +7,9 @@ $senha = "6#vWHD_$";
 $porta = "50526";
 
 try {
-    $conexao = new PDO("mysql:host=$servidor;port=$porta;dbname=$banco;charset=utf8", $banco, $senha);
+    $conexao = new PDO("mysql:host=$servidor;port=$porta;dbname=$banco;charset=utf8", $usuario, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $excecao) {
-    echo "Erro ao conectar ao banco: $excecao";
+    echo "Erro ao conectar ao banco: $excecao->errorInfo";
 }
