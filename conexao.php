523e5915e20cf65bbf5481ee0ac915feca744e48
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-27 00:32:49 -0300

    Adição da porta para a conexão

--- conexao.php
+++ conexao.php
@@ -7,7 +7,7 @@ $senha = "6#vWHD_$";
 $porta = "50526";
 
 try {
-    $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $banco, $senha);
+    $conexao = new PDO("mysql:host=$servidor;port=$porta;dbname=$banco;charset=utf8", $banco, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $excecao) {
