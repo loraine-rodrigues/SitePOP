2644414145ee41cb4e63d5922c7f18ca20b3577d
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-04-27 00:24:22 -0300

    Atualização do arquivo de conexão

--- conexao.php
+++ conexao.php
@@ -9,7 +9,7 @@ $porta = "50526";
 try {
     $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $banco, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
+}
 catch (PDOException $excecao) {
     echo "Erro: $excecao->errorInfo";
 }
