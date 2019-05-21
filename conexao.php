1605f517da3b9f7dc15f03f4728997cfbda4823a
Thereza Loraine <49696790+loraine-rodrigues@users.noreply.github.com>
2019-05-21 18:37:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,14 +2,14 @@
 
 $servidor = "localhost";
 $banco = "motofrete";
-$usuario = "root";
-$senha = "";
-$porta = "3306";
+$usuario = "azure";
+$senha = "6#vWHD_$";
+$porta = "50526";
 
 try {
     $conexao = new PDO("mysql:host=$servidor;port=$porta;dbname=$banco;charset=utf8", $usuario, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $excecao) {
-    echo "Erro ao conectar ao banco: " . $excecao->getMessage();
+    echo "Erro ao conectar ao banco: $excecao->errorInfo";
 }
