d5fd73fd6750201ee0f3294d05488a0b7a8ae848
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-20 22:33:13 -0300

    Update home.php

--- home.php
+++ home.php
@@ -41,6 +41,8 @@ include 'header.php' ?>
         width:100%;
         background-image: url(image/home4.jpg) !important;
         background-repeat: no-repeat;
+        background-position: 30% 45%;
+        background-size: cover;
         
         opacity: 1.0;
         filter: alpha(opacity=80);
