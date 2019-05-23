6fbfdaf4f653d5455bab64eb2e998f14873b3455
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-22 23:55:23 -0300

    Add files via upload

--- duvidas.php
+++ duvidas.php
@@ -3,102 +3,141 @@ $title = "DÚVIDAS";
 
 include 'header.php' ?>
 
-        <div class="container text-center">
-            <h2 class="font-weight-light text-white ml-5">DÚVIDAS<img src="duvida.svg" width="150" height="150" class="align-middle ml-4"> </h2>
-            <hr/>
 
-            <div class="accordion mb-3">
-                                         <!--DÚVIDA 1-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta1">
-                                Dúvida 1
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta1" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                                         <!--DÚVIDA 2-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta2">
-                                Dúvida 2
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta2" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                                    <!--DÚVIDA 3-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta3">
-                                Dúvida 3
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta3" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                                    <!--DÚVIDA 4-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta4">
-                                Dúvida 4
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta4" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                                    <!--DÚVIDA 5-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta5">
-                                Dúvida 5
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta5" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                                    <!--DÚVIDA 6-->
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pergunta6">
-                                Dúvida 6
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="pergunta6" class="collapse">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
+
+<div class="container text-center">
+    
+    <h2 class="font-weight-light text-white ml-5">DÚVIDAS<img src="image/duvidas.png" width="80" height="80" class="align-middle ml-4"> </h2>
+    <hr />
+
+    <div class="accordion mb-3">
+        <!--DÚVIDA 1-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta1">
+                        <img src="image/duvida1.png" class="mr-3" width="50" height="50">
+                        O serviço do POP! é gratuito?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta1" class="collapse">
+                <div class="card-body">
+                    O serviço de cadastros para motofretistas e clientes é gratuito.
+                    Se você tiver interesse em maiores recursos na plataforma entre em contato conosco e faça sua sugestão ;)
+
+                    -Só há pagamento de serviços na plataforma POP! caso você se cadastre em algum Plano PREMIUM que oferece maiores recursos na plataforma.
+
                 </div>
             </div>
         </div>
+        <!--DÚVIDA 2-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta2">
+                        <img src="image/credit-card.svg" width="50" height="50" class="mr-3">
+                        Onde pago pelo serviço do motofretista?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta2" class="collapse">
+                <div class="card-body">
+                    As negociações sobre a entrega e valores são feitas externamente entre você e o motofretista,
+                    através do contato fornecido por ele(a) na página (colocar link), sem interferência da plataforma POP!
+                </div>
+            </div>
+        </div>
+        <!--DÚVIDA 3-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta3">
+                        <img src="image/duvida3.png" width="50" height="50" class="mr-3">
+                        Porque preciso me cadastrar?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta3" class="collapse">
+                <div class="card-body">
+                    Para obter maiores informações profissionais sobre o motofretista é necessário cadastro.
+                    Assim promovemos ao motofretista mais segurança para exibir os dados apenas para pessoas que fazem parte do POP!
+                </div>
+            </div>
+        </div>
+        <!--DÚVIDA 4-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta4">
+                        <img src="image/duvida4.png" width="50" height="50" class="mr-3">
+                        O que o site oferece?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta4" class="collapse">
+                <div class="card-body">
+                    O POP! oferece a motofretistas um meio para divulgarem suas informações profissionais de forma organizada e concentrada.
+                    Assim facilitamos a busca para clientes que precisam do serviço de motofrete. </div>
+            </div>
+        </div>
+        <!--DÚVIDA 5-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta5">
+                        <img src="image/duvida5.png" width="50" height="50" class="mr-3">
+                        E os valores das entregas?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta5" class="collapse">
+                <div class="card-body">
+                    Você pode negociar da maneira que achar melhor! O POP! acredita que você e o motofretista podem chegar a um acordo que seja vantajoso para ambos. Dessa forma permitimos a livre negociação entre vocês, sem taxas e prazos.
+
+                    -O POP! não interfere nas negociações. Qualquer transação de valores, negociação e pagamentos são de responsabilidade do Cliente e Motofretista.
+
+                </div>
+            </div>
+        </div>
+        <!--DÚVIDA 6-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta6">
+                        <img src="image/duvidas6.png" width="50" height="50" class="mr-3">
+                        É seguro?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta6" class="collapse">
+                <div class="card-body">
+
+                    Para se assegurarmos as informações fornecidas na plataforma solicitamos ao motofretista a documentação específica exigida pelo site e pela lei(colocar link) que regulamenta a profissão de motofrete.
+
+                    O pop! verifica e valida o cadastro após esse procedimento. Assim proporcionamos maior segurança para as pessoas que se cadastram e se conectam com os motofretistas para realizar as entregas.
+                </div>
+            </div>
+        </div>
+        <!--DÚVIDA 7-->
+        <div class="card">
+            <div class="card-header">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-warning" type="button" data-toggle="collapse" data-target="#pergunta7">
+                        <img src="image/duvidas7.png" width="50" height="50" class="mr-3">
+                        Quem tem acesso as informações cadastradas no POP! ?
+                    </button>
+                </h2>
+            </div>
+            <div id="pergunta7" class="collapse">
+                <div class="card-body">
+                    Aqueles que se cadastram no site tem acesso às informações públicas do motofretista. Além do Nome e contato,
+                    as pessoas cadastradas podem visualizar a região de atendimento, foto, e placa do veículo.
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include 'footer.php' ?>
--- sobreNos.php
+++ sobreNos.php
@@ -3,18 +3,41 @@ $title = "SOBRE NÓS";
 
 include 'header.php' ?>
 
-        <div class="container text-center">
-            <h2>SOBRE NÓS</h2>
-			O POP! é uma plataforma com o objetivo de promover ideias de tecnologia
-para integração digital de autônomos. Para atender as demandas de
-mobilidade e rapidez no estilo de vida urbano surgiu o POP! Motofrete.
-Nossa missão é facilitar e divulgar os prestadores de serviço para que sejam
-protagonistas do seu negócio. E permitir que clientes que buscam por essa
-mão de obra tenham maior comodidade acessando o POP! para encontrá-los.
-Priorizamos a transparência, ética e eficiência para que todos possam
-aproveitar o melhor dessa ferramenta.
-Comece agora mesmo! CADASTRE-SE no POP! para escolher o Motofretista
-ideal para realizar serviços de entregas, retiradas, recebimentos e muito mais!
+<style type="text/css">
+    .ac_box {
+        max-width: 100% width: 1600px;
+        border: 0px solid #E6E6E6;
+        border: 0px solid #E6E6E6;
+        height: 300px;
+        padding: 1px 0;
+        background-image: url(image/home3.jpg) !important;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+
+
+<div class="ac_box">
+
+</div>
+
+
+<div class="card m-auto text-left" style="width: 80rem;">
+
+    <div class="card-body">
+        <div class="text-center">
+            <h2 class="ml-5">SOBRE NÓS</h2>
+            O POP! é uma plataforma com o objetivo de promover ideias de tecnologia para integração digital de autônomos.
+            Para atender as demandas de mobilidade e rapidez no estilo de vida urbano surgiu o POP! Motofrete.
+            Nossa missão é facilitar e divulgar os prestadores de serviço para que sejam protagonistas do seu negócio.
+            E permitir que clientes que buscam por essa mão de obra tenham maior comodidade acessando o POP! para encontrá-los.
+            Priorizamos a transparência, ética e eficiência para que todos possam aproveitar o melhor dessa ferramenta.
+            Comece agora mesmo! CADASTRE-SE no POP! para escolher o Motofretista ideal para realizar serviços de entregas, retiradas,
+            recebimentos e muito mais!
         </div>
+    </div>
+</div>
+
+
 
 <?php include 'footer.php' ?>
