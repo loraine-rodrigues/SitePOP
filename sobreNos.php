dc7f47af3bc659547b17dd624686ba04e089f2cb
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-27 20:35:52 -0300

    Update sobreNos.php

--- sobreNos.php
+++ sobreNos.php
@@ -5,7 +5,7 @@ include 'header.php' ?>
 
 <div class="container text-center mb-3">
 
-<div class="card m-auto" style="width: 45rem;">
+<div class="card m-auto" style="width: 35rem;">
 
     <div class="card-body">
         <div class="text-center">
