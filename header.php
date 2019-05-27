4b51ba0810019fe75bf852966ea137c99d1540e0
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-27 20:46:02 -0300

    Update header.php

--- header.php
+++ header.php
@@ -64,7 +64,7 @@ echo '
     <header>    
         
        <!--INICIO da navbar-->
-        <nav class="navbar navbar-expand-lg navbar-customizada navbar-dark"> 
+        <nav class="navbar navbar-expand-lg navbar-customizada navbar-light"> 
         <a class="navbar-brand" href="/index.php">POP!</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
