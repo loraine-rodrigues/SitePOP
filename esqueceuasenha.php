cf41cabd3257a827526245ee862184f93bd28d17
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-28 19:18:20 -0300

    Update esqueceuasenha.php

--- esqueceuasenha.php
+++ esqueceuasenha.php
@@ -3,48 +3,46 @@ $title = "ESQUECI MINHA SENHA";
 include 'conexao.php';
 include 'header.php';
 
-if (isset($_POST['ok'])) {
-    $email = $_POST['email'];
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $erro[] = "E-mail inválido.";
-    }
-    try {
-        $comando = $conexao->prepare("CALL verificaLogin(?, ?)");
-        $comando->bindParam(1, $email);
-        $comando->bindParam(2, $senha);
-        $comando->execute();
-        if ($comando->rowCount() > 0) {
-
-            while ($resultado = $comando->fetch(PDO::FETCH_ASSOC)) {
-
-                $_SESSION['nome'] = $resultado['nm_usuario'];
-                $novaSenha = substr(md5(time()), 0, 6);
-                $nscriptografada = md5(md5($novaSenha));
-            }
+if(isset($_POST['recuperando']) && $_POST['recuperando'] == 'rec'){
+	$email =    strip_tags(trim($_POST['email']));
+   
+  //busca no banco o usuario com o email
+    $sql = "SELECT * FROM tb_login WHERE nm_usuario = '$email'";
+ 
+    $res = $pdo->query($sql);
+ 
+    // caso haja mais de um cadastrado...
+    if($res->rowCount() == 1){
+        //fazemos um while para coletarmos as outras informações do usuario
+        //tais como o nome e a senha
+        while($exibir = $res->fetch(PDO::FETCH_OBJ)){
+            $nome = $exibir->nome;
+            $senha = $exibir->senha;
         }
-    } catch (PDOException $excecao) {
-        echo "Erro: " . $excecao->getMessage();
+        
+        //envia o email para a pessoa juntamente com seu nome e sua senha
+        $msg="Olá $nome, você solicitou a recuperação de senha.\n";
+        $msg.="Sua senha é: $senha";
+        
+        mail($email, "Recuperação de senha", $msg);
+        
+        //alerta que o email foi enviado e o redireciona para outra página
+        echo"<script>alert('Senha enviada por e-mail, verifique sua caixa de mensagens ou sua caixa de spans.'),window.open('login.php','_self')</script>";
     }
-
-    if (mail($email, "Sua nova senha", "Sua nova senha: " . $novaSenha)) {
-        $sql_code = "UPDATE tb_login SET id_senha = 'nscriptografada' where nm_usuario= '$email'";
-        $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+    //caso contrário
+    else{
+        //lhe informa que o seu e-mail não está cadastrado no banco de dados
+        echo"<script>alert('E-mail não cadastrado em nosso sistema, caso não se lembre do email cadastrado, entre em contato conosco.'),window.open('recuperarSenha.php','_self')</script>";
+        
     }
+   
 }
 
-?>
-<?php
-if (isset($_SESSION['erro'])) :
-    ?>
-    <div class="alert alert-danger">
-        <?php echo $_SESSION['erro']; ?>
-    </div>
-<?php
-endif;
-session_destroy();
+
 ?>
 <form method="POST" action="">
     <input placeholder="Seu email" name="email" type="text">
-    <input name="ok" value="ok" type="submit">
+    <input name="recuperando" value="Enviar" type="submit">
 
-</form>
+<?php include 'footer.php';
+?>
