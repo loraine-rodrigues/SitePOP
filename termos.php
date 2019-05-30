8a8157896c4a4a24f3bb9940f37ce65496c92c6f
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-30 11:28:27 -0300

    Update termos.php

--- termos.php
+++ termos.php
@@ -29,17 +29,17 @@ body {
             
             <img src="image/termos.png" width="150" height="150" class="float-right">
             O presente termo regulará as operações e prestações de serviços entre a POP! Motofrete, seus usuários e associados, pessoas jurídicas ou físicas, em todo território nacional, sendo a sua observância obrigatória para fruição do sistema.
-  
+  <br> 
 1 – PROPOSTA DO SISTEMA
 O POP! Motofrete disponibilizará o seu sistema aos seus usuários e associados para fins de contratação do serviço de motofrete, por meio de seleção do motofretista e cotação, feito externamente através  dos contatos profissionais fornecidos pelo prestador de serviço na plataforma. 
- 
+
 Mediante a escolha do perfil dos cadastrados, pelos usuários e pelos associados, ambos terão a possibilidade de negociarem a  cotação com diversos participantes, cujo resultado  será a os ajustes de acordo a necessidade de cada entrega e cada cliente.
- 
+ <br> 
 1. 1 PRINCIPAIS CARACTERÍSTICAS DO SISTEMA:
  
 Perfil completo dos prestadores de transportes, dados do veículo,  localidades atendidas, bem como os meio para contato: telefone com aplicativo para mensagens instantâneas (whats’app) e telefone de emergência. 
 Diversos clientes cadastrados e com interesse na contratação de serviços, disponibilizando as principais informações de nome e telefone.
- 
+ <br> 
 1.2 ALGUMAS VANTAGENS DO SISTEMA
  
 O sistema irá exibir os dados do motofretista de acordo com o filtro de região que o usuário selecionar, possibilitando aos usuários e aos associados traçar o perfil do proponente para fins de pesquisa no nosso vasto banco de dados, contando com diversos  prestadores de serviços.
@@ -51,11 +51,11 @@ O sistema  não irá interferir nas cotações, valores e formas de pagamento. F
 No caso do usuário fazer o pagamento pontual e diretamente ao prestador de serviço, o POP! Motofrete não terá nenhuma responsabilidade, inclusive de natureza tributária e fiscal. Nesta modalidade de contratação, tanto prestador de serviços quanto cliente ficam responsáveis por acordarem o local, formas de pagamento, valores, e se houver,  taxas e juros pela cobrança da conclusão do serviço de entregas. 
 O sistema possui restrições para o cadastro dos usuários e dos associados, o que garantirá aos interessados uma seleção de clientes e de prestadores de serviços com qualidade e credibilidade.
  A indicação  e divulgação do proponente dos serviços ao cliente não configura nenhuma contratação, sendo está efetivada somente após o aceite do contrato celebrado entre as partes.
- 
+ <br> 
 2 – MANUTENÇÃO DO SISTEMA
  
 O sistema será periodicamente revisado para melhoria da sua funcionalidade, por esta razão, poderão ocorrer paralizações para manutenção, fora do horário comercial, sem prévio aviso.
- 
+ <br> 
 3 – UTILIZAÇÃO DO SISTEMA
  
 Os participantes do sistema, usuário ou associado, aceitam todos os termos e as condições aqui estabelecidas de forma livre e espontânea vontade, sendo, exclusivamente, responsáveis pelas informações prestadas, bem como pelos compromissos assumidos de seleção, negociação e recebimento de valores e itens.
@@ -65,40 +65,40 @@ Os usuários e os participantes se comprometem a utilizar o sistema com observâ
 É terminantemente proibido, ao usuário e ao associado, ao uso indevido das informações obtidas no sistema, bem como a cópia e a reprodução, incluindo a distribuição ou divulgação, de conteúdo do site.
  
 O usuário prestador do serviço poderá, por meio do seu perfil gerado com as informações fornecidas por ele, alterar informações sobre seu cadastro e veículo. No entanto, os dados do veículo passarão por solicitação da documentação atual e revalidação pelo  POP! Motofrete. 
- 
+ <br> 
 4 – DIREITOS AUTORAIS E MARCAS RESERVADOS AO POP! Motofrete 
  
 O sistema é protegido pela Lei nº 9.610/98, sendo as suas informações e produtos de uso exclusivo do  POP! Motofrete, sendo, em caso de desrespeito a esta Lei, passível de sanção cível e penal.
  
 A proprietária do sistema não cede ou transferência nenhuma autorização uso ou exploração dos direitos reservados a ela.
- 
+ <br> 
 5 – DISPONIBILIDADE DO SISTEMA E DOS SEUS CONTEÚDOS
  
 A proprietária poderá interromper os serviços em caso de manutenção ou fatores alheios à sua vontade, não gerando ao usuário ou ao associado qualquer direito à indenização, seja a que título for.
- 
+ <br> 
 6 – PROTEÇÃO DO SISTEMA
  
 Para evitar qualquer dano ao sistema, os usuários e os associados se comprometem a utilizar antivírus, versão mais atualizada. Além de manterem suas informações restritas apenas ao seu uso pessoal, não fornecendo sua senha e e-mail para terceiros.
- 
+ <br> 
 7 – RESPONSABILIDADE
  
 O uso das informações disponibilizadas no sistema não dará direito à indenização de qualquer natureza, seja a que título for e em qualquer tempo.
  
 O POP! Motofrete não se responsabilizará pelos negócios gerados pelo sistema entre o prestador se serviços e o contratante nem a carga transportada. Já que toda a negociação se passa externamente, sem que haja qualquer fator que relacione dependência da plataforma para verificação da entrega.
- 
+ <br> 
 8 – GARANTIA DE ENTREGA
  
 A plataforma se isenta de oferecer  serviço de garantia de entrega, na qual o POP! Motofrete não se responsabilizará por eventuais perdas ocorridas.
 Colaboraremos com eventuais consultas de dados e informações sobre o cadastro do motofretista para que haja transparência e ausência de equívocos no ínterim da entrega. Para que assim, seja evitado danos por insuficiência de informações do prestador de serviço. 
- 
+ <br> 
 9 – RESTRIÇÃO AO USO DESTE WEBSITE
 
 O POP! Motofrete, em função de uso irregular do sistema e dos seus conteúdos, reserva-se no direito de recusar ou retirar o acesso do usuário ou do associado, sem aviso prévio, em caso de fraude, simulação, determinação judicial ou identificação de atividades irregulares.
- 
+ <br> 
 10 – VIGÊNCIA
  
 O prazo de duração deste sistema é indeterminado.
- 
+ <br> 
 11 –PLANO PREMIUM(monetização do site - Forma 1)
 Em elaboração
  Em caso de opção do usuário pela contratação do PLANO PREMIUM que oferece recursos extras na plataforma como: xxx xxx xxx xxxx xxx , será selecionado o  sistema de pagamento mensal ao  POP! Motofrete por meio de ______________  todo dia _____. 
@@ -106,17 +106,17 @@ Em elaboração
 O valor da taxa de serviço Premium pelo uso do sistema do POP! Motofrete 
 ________________poderá ser conferido antes do aceite do serviço de cadastro nessa modalidade, sendo da sua única e exclusiva vontade acerca da aceitação ou não proposta de serviços. Esse valor também poderá sofrer alterações e /ou promoções com aviso prévio ao usuário cadastrado no plano para que fique ciente de qualquer mudança. 
 O usuário poderá cancelar a qualquer momento o Plano Premium, ou acrescentar novos recursos. Em caso de escolha das opções de cancelamento ou migração o usuário se compromete a efetuar o pagamento do período utilizado. 
- 
+ <br> 
 12 – DISPOSIÇÕES GERAIS
  
 O uso do sistema não gera qualquer relação de dependência entre os usuários ou com o POP! Motofrete, nem vínculo de emprego.
- 
+ <br> 
 13 – FORO DE ELEIÇÃO
  
 O usuário e o associado aceita a eleição do Foro Central da Comarca da Capital do Estado de São Paulo, com exclusão de qualquer outro por mais privilegiado que seja.
  
 
-
+<br> 
           
         </div>
     </div>
