81caccb3d01a869ab9020a3e07f277710a04eabd
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-05-30 15:09:18 -0300

    Update header.php

--- header.php
+++ header.php
@@ -73,7 +73,7 @@ echo '
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
              <li class="nav-item mx-2">
-                <a class="nav-link" href="/catalogo.php">Motofretistas</a>
+                <a class="nav-link" href="/catalogo.php">Catálogo</a>
               </li> 
               <li class="nav-item mx-2">
                 <a class="nav-link" href="/sobreNos.php">Sobre nós</a>
