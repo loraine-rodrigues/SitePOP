4200f6e52972ae011acf0f3015971cb6f77db2ad
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-05 11:56:18 -0300

    Add files via upload

---
+++ comoFunciona.php
@@ -0,0 +1,135 @@
+<?php
+$title = "COMO FUNCIONA";
+require 'conexao.php';
+include 'header.php';
+?>
+<!-- Projects section v.3 -->
+<section class="my-5">
+<h1 class="h1-responsive text-center my-5">COMO FUNCIONA?</h1>
+<hr>
+  <!-- Section heading -->
+  <h2 class="h1-responsive font-weight-bold text-center my-5">Clientes</h2>
+  <!-- Section description -->
+  <p class="grey-text text-center w-responsive mx-auto mb-5">Veja como é rápido e fácil utilizar nossa plataforma</p>
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-5 mb-lg-0 mb-5">
+      <!--Image-->
+      <img src="image/foto2.jpg" alt="Sample project image" class="img-fluid rounded z-depth-1">
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Grid row -->
+      <div class="row mb-3">
+        <div class="col-md-1 col-2">
+          <i class="fa fa-id-card fa-2x cyan-text"></i>
+        </div>
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Efetue seu cadastro</h5>
+          <p class="grey-text">Assim você terá total acesso a plataforma e aos Motofretistas.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+      <!-- Grid row -->
+      <div class="row mb-3">
+        <div class="col-md-1 col-2">
+          <i class="fa fa-user-circle fa-2x red-text"></i>
+        </div>
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Escolha um Motofretista</h5>
+          <p class="grey-text">Você poderá visualizar e escolher o motofretista de sua preferência.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+      <!-- Grid row -->
+      <div class="row">
+        <div class="col-md-1 col-2">
+          <i class="fa fa-check fa-2x deep-purple-text"></i>
+        </div>
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Entre em contato</h5>
+          <p class="grey-text mb-0">Faça contato com o motofretista de forma rápida e negocie da forma que desejar.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+  <h2 class="h1-responsive font-weight-bold text-center my-5">Motofretistas</h2>
+  <hr class="my-5">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Grid row -->
+      <div class="row mb-3">
+        <div class="col-md-1 col-2">
+          <i class="fa fa-id-card fa-2x indigo-text"></i>
+        </div>
+        
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Efetue seu cadastro</h5>
+          <p class="grey-text">Cadastre-se na plataforma, assim você poderá divulgar seus serviços através de seu perfil visível para 
+            os clientes.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+      <!-- Grid row -->
+      <div class="row mb-3">
+        <div class="col-md-1 col-2">
+          <i class="fa fa-user-circle fa-2x pink-text"></i>
+        </div>
+        
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Perfil</h5>
+          <p class="grey-text">Tenha acesso ao seu perfil com suas informações, podendo editá-las a qualquer momento.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+      <!-- Grid row -->
+      <div class="row mb-lg-0 mb-5">
+        <div class="col-md-1 col-2">
+          <i class="far fa-grin fa-2x blue-text"></i>
+        </div>
+        <div class="col-md-11 col-10">
+          <h5 class="font-weight-bold mb-3">Aguarde contato</h5>
+          <p class="grey-text mb-0">Aguade até seu perfil ser visualizado e o cliente entre em contato com você através do número de WhatsApp fornecido.</p>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+      <!--Image-->
+      <img src="image/foto.jpg" alt="Sample project image" class="img-fluid rounded z-depth-1">
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</section>
+<!-- Projects section v.3 -->
+  <?php
+  include 'footer.php';
+  
+  
