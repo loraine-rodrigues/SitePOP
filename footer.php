0c861870a9abe23559ac3f4f8de8944fca250057
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-06 11:51:53 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,29 +3,29 @@
 echo '
     </main>
     
-    <footer class="mt-auto py-3 text-center bg-light" >
+    <footer class="mt-auto p-3 text-center bg-light" >
     
       <div class="container ">
         <span class="text-dark-50";   >&copy; 2019 - POP!</span>
         
       </div>
     
-          
+      <div class="container-fluid bg-light mt-3">
+      <ul class="list-unstyled ">
+      <li>
+                  <a class="text-dark" href="/duvidas.php">Dúvidas Frequentes</a>
+                </li>
+      <li>
+        <a class="text-dark" href="sobreNos.php">Sobre nós</a>
+      </li>
+      <ul class="list-unstyled">
+      <li>
+      <a class="text-dark" href="termos.php">Termos de uso</a>
+      </li>
+      
+      </div> 
     </footer>
-    <div class="container-fluid bg-light ">
-    <ul class="list-unstyled ">
-    <li>
-                <a class="text-dark" href="/duvidas.php">Dúvidas Frequentes</a>
-              </li>
-    <li>
-      <a class="text-dark" href="sobreNos.php">Sobre nós</a>
-    </li>
-    <ul class="list-unstyled">
-    <li>
-    <a class="text-dark" href="termos.php">Termos de uso</a>
-    </li>
     
-    </div> 
     <script>
         $(document).ready(function() {
             $(\'a[href="\' + this.location.pathname + \'"]\').parent().addClass(\'active\');
