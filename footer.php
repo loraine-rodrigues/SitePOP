4473c2cca03fd13b94aec013ff09947579b7e141
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-06 11:46:06 -0300

    links no rodapé
    alguns links da barra de navegação estão agora no rodapé. Não sei se é possivel ficar visualmente bom, mas foi o que consegui mudar

--- footer.php
+++ footer.php
@@ -2,12 +2,30 @@
 
 echo '
     </main>
-    <footer class="footer mt-auto py-3 text-center bg-light navbar-customizada";   >
-      <div class="container">
-        <span class="text-black-50";   >&copy; 2019 - POP!</span>
+    
+    <footer class="mt-auto py-3 text-center bg-light" >
+    
+      <div class="container ">
+        <span class="text-dark-50";   >&copy; 2019 - POP!</span>
         
       </div>
+    
+          
     </footer>
+    <div class="container-fluid bg-light ">
+    <ul class="list-unstyled ">
+    <li>
+                <a class="text-dark" href="/duvidas.php">Dúvidas Frequentes</a>
+              </li>
+    <li>
+      <a class="text-dark" href="sobreNos.php">Sobre nós</a>
+    </li>
+    <ul class="list-unstyled">
+    <li>
+    <a class="text-dark" href="termos.php">Termos de uso</a>
+    </li>
+    
+    </div> 
     <script>
         $(document).ready(function() {
             $(\'a[href="\' + this.location.pathname + \'"]\').parent().addClass(\'active\');
