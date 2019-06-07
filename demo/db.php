9d63fc31e9df3fac108da620806c4155f33af68b
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-07 11:49:15 -0300

    Recuperação de senha
    Problema na hora de alterar a senha no banco.

---
+++ demo/db.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Enter your Host, username, password, database below.
+// I left password empty because i do not set password on localhost.
+$con = mysqli_connect("localhost","root","","motofrete");
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+		}
+
+date_default_timezone_set('Asia/Karachi');	
+$error="";	
+?>
