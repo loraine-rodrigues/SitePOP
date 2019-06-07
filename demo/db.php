c55484e9c256424b574f372deee8dfa5819e42ed
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-07 20:11:52 -0300

    Update db.php

--- demo/db.php
+++ demo/db.php
@@ -2,8 +2,8 @@
 
 
 // Enter your Host, username, password, database below.
-$porta = "50526";
-$con = mysqli_connect("localhost",$porta,"azure","6#vWHD_$","motofrete");
+
+$con = mysqli_connect("localhost","50526","azure","6#vWHD_$","motofrete");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
