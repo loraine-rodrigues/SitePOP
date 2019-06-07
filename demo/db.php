2dd4f2a4e5ac98bd776c2433697e023f70ada7f3
Leticia Sampaio <49662997+lefsampaio@users.noreply.github.com>
2019-06-07 11:51:43 -0300

    Update db.php

--- demo/db.php
+++ demo/db.php
@@ -2,8 +2,8 @@
 
 
 // Enter your Host, username, password, database below.
-// I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","motofrete");
+
+$con = mysqli_connect("localhost","azure","6#vWHD_$","motofrete");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
