3c6e8960614c8dcd936b76c685f2e4896944f452
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-06-09 22:44:08 -0300

    Atualização das procedures de motofretista e cliente

--- BancoDeDados/ProcedureMotofretistas.sql
+++ BancoDeDados/ProcedureMotofretistas.sql
@@ -7,30 +7,30 @@ drop procedure if exists cadastrarMotofretista;
 
 DELIMITER $$
 CREATE PROCEDURE `cadastrarMotofretista` (
- IN nome varchar(100),
- IN celular char(11), 
- IN telefone char(11),
- IN email varchar(100),
- IN cpf char(11),
- IN cnpj char(14),
- IN cnh char(11),
- IN genero enum ('Masculino','Feminino','Outros'),
- in regiao set ('Bertioga', 'Cubatão', 'Guarujá', 'Itanhaém', 'Mongaguá', 'Peruíbe', 'Praia Grande', 'Santos', 'São Vicente'),
- IN nascimento DATE,
- IN mei enum ('Sim', 'Não'),
- IN placa char(7),
- IN renavam char(11),
- IN modelo varchar(45),
- IN cor varchar(45),
- IN marca varchar(45),
- IN senha varchar(45),
- IN foto varchar(100))
- 
+    IN nome varchar(100),
+    IN celular char(11),
+    IN telefone char(11),
+    IN email varchar(100),
+    IN cpf char(11),
+    IN cnpj char(14),
+    IN cnh char(11),
+    IN genero enum ('Masculino','Feminino','Outros'),
+    in regiao set ('Bertioga', 'Cubatão', 'Guarujá', 'Itanhaém', 'Mongaguá', 'Peruíbe', 'Praia Grande', 'Santos', 'São Vicente'),
+    IN nascimento DATE,
+    IN mei enum ('Sim', 'Não'),
+    IN placa char(7),
+    IN renavam char(11),
+    IN modelo varchar(45),
+    IN cor varchar(45),
+    IN marca varchar(45),
+    IN senha varchar(45),
+    IN foto varchar(100))
+
 BEGIN
-	 INSERT INTO tb_login (nm_usuario, id_senha, id_tipo_login)
-		VALUES (email, md5(senha), '2');
-	INSERT INTO tb_motofretista (nm_motofretista, id_celular, id_telefone, nm_email, id_cpf, id_cnpj, id_cnh, ic_genero, nm_regiao, dt_nascimento, ic_mei, id_placa, id_renavam, nm_modelo, nm_cor, nm_marca, urlFoto)
-		VALUES (nome, celular, telefone, email, cpf, cnpj, cnh, genero, regiao, nascimento, mei, placa, renavam, modelo, cor, marca, foto);
+    INSERT INTO tb_login (nm_usuario, nm_login, id_senha, id_tipo_login)
+    VALUES (nome, email, md5(senha), '2');
+    INSERT INTO tb_motofretista (nm_motofretista, id_celular, id_telefone, nm_email, id_cpf, id_cnpj, id_cnh, ic_genero, nm_regiao, dt_nascimento, ic_mei, id_placa, id_renavam, nm_modelo, nm_cor, nm_marca, urlFoto)
+    VALUES (nome, celular, telefone, email, cpf, cnpj, cnh, genero, regiao, nascimento, mei, placa, renavam, modelo, cor, marca, foto);
 END$$
 DELIMITER ;
 
@@ -40,29 +40,30 @@ drop procedure if exists editarMotofretista;
 
 DELIMITER $$
 create procedure `editarMotofretista` (
- IN id int (11),
- IN nome varchar(100),
- IN celular char(11), 
- IN telefone char(11),
- IN email varchar(100),
- IN cpf char(11),
- IN cnpj char(14),
- IN cnh char(11),
- IN genero varchar(9),
- in regiao varchar(11),
- IN nascimento DATE,
- IN mei enum ('Sim', 'Não'),
- IN placa char(7),
- IN renavam char(11),
- IN modelo varchar(45),
- IN cor varchar(45),
- IN marca varchar(45),
- IN foto varchar(100))
+    IN id int (11),
+    IN nome varchar(100),
+    IN celular char(11),
+    IN telefone char(11),
+    IN email varchar(100),
+    IN cpf char(11),
+    IN cnpj char(14),
+    IN cnh char(11),
+    IN genero varchar(9),
+    in regiao varchar(11),
+    IN nascimento DATE,
+    IN mei enum ('Sim', 'Não'),
+    IN placa char(7),
+    IN renavam char(11),
+    IN modelo varchar(45),
+    IN cor varchar(45),
+    IN marca varchar(45),
+    IN foto varchar(100))
 
- BEGIN
-	 UPDATE tb_motofretista SET nm_motofretista = nome, id_celular = celular, id_telelefone = telefone, nm_email = email, id_cpf = cpf, id_cnpj = cnpj, id_cnh = cnh, ic_genero = genero,
-	 nm_regiao = regiao, dt_nascimento = nascimento, ic_mei = mei, id_placa = placa, id_renavam = renavam, nm_modelo = modelo, nm_cor = cor, nm_marca = marca, urlFoto = foto where id_motofretista = id;
- END $$
+BEGIN
+    UPDATE tb_login SET nm_login = email, nm_usuario = nome;
+    UPDATE tb_motofretista SET nm_motofretista = nome, id_celular = celular, id_telefone = telefone, nm_email = email, id_cpf = cpf, id_cnpj = cnpj, id_cnh = cnh, ic_genero = genero,
+                               nm_regiao = regiao, dt_nascimento = nascimento, ic_mei = mei, id_placa = placa, id_renavam = renavam, nm_modelo = modelo, nm_cor = cor, nm_marca = marca, urlFoto = foto where id_motofretista = id;
+END $$
 DELIMITER ;
 
 
@@ -73,7 +74,7 @@ DELIMITER $$
 create procedure `buscarMotofretista` (in id int (11))
 begin
 
-	select * from tb_motofretista where id_motofretista = id;
+    select * from tb_motofretista where id_motofretista = id;
 
 end $$
 DELIMITER ;
@@ -85,20 +86,20 @@ DELIMITER $$
 create procedure `buscarMotofretistas` ()
 begin
 
-	select * from tb_motofretista;
+    select * from tb_motofretista;
 
 end $$
 DELIMITER ;
 
 
--- DELETAR MOTOFRETISTA 
+-- DELETAR MOTOFRETISTA
 drop procedure if exists deletarMotofretista;
 
 DELIMITER $$
 create procedure `deletarMotofretista` (in id int(11))
 begin
-	delete from tb_login where nm_usuario = (SELECT nm_email from tb_motofretista WHERE id_motofretista = id);
-	delete from tb_motofretista where id_motofretista = id;
+    delete from tb_login where nm_login = (SELECT nm_email from tb_motofretista WHERE id_motofretista = id);
+    delete from tb_motofretista where id_motofretista = id;
 
 end $$
 DELIMITER ;
--- BancoDeDados/ProceduresClientes.sql
+++ BancoDeDados/ProceduresClientes.sql
@@ -7,14 +7,13 @@ USE `motofrete`;
 DROP procedure IF EXISTS `cadastrarCliente`;
 
 DELIMITER $$
-USE `motofrete`$$
 CREATE PROCEDURE `cadastrarCliente` (IN nome varchar(255), IN nascimento date, IN cpf char(11), IN email varchar(100), IN celular varchar(11), IN senha varchar(45))
 BEGIN
-	INSERT INTO tb_login (nm_usuario, id_senha, id_tipo_login)
-		VALUES (email, md5(senha), '1');
+	INSERT INTO tb_login (nm_usuario, nm_login, id_senha, id_tipo_login)
+		VALUES (nome, email, md5(senha), '1');
 	INSERT INTO tb_cliente (nm_cliente, dt_nascimento, id_cpf, nm_email, cd_celular)
 		VALUES (nome, nascimento, cpf, email, celular);
-	
+
 END$$
 
 DELIMITER ;
@@ -23,10 +22,11 @@ DELIMITER ;
 -- EDITAR MOTOFRETISTA
 drop procedure if exists `editarCliente`;
 
-DELIMITER $$ 
+DELIMITER $$
 create procedure editarCliente (in id int(11), IN nome varchar(255), IN nascimento date, IN cpf char(11), IN email varchar(100), IN celular varchar(11))
 
 begin
+  update tb_login set nm_login = email, nm_usuario = nome;
 	update tb_cliente set nm_cliente = nome, dt_nascimento = nascimento, id_cpf = cpf, nm_email = email, cd_celular = celular where id_cliente = id;
 end $$
 DELIMITER ;
@@ -41,7 +41,7 @@ create procedure buscarCliente (in id int(11))
 begin
 	select * from tb_cliente where id_cliente = id;
 end $$
-DELIMITER ; 
+DELIMITER ;
 
 -- BUSCAR TODOS CLIENTES
 drop procedure if exists `buscarClientes`;
@@ -54,14 +54,14 @@ begin
 end $$
 DELIMITER ;
 
--- DELETAR CLIENTE  
+-- DELETAR CLIENTE
 drop procedure if exists deletarCliente;
 
 DELIMITER $$
 create procedure deletarCliente(in id int)
 
 begin
-	delete from tb_login where nm_usuario = (SELECT nm_email from tb_cliente WHERE id_cliente = id);
-    delete from tb_cliente where id_cliente = id;
+	delete from tb_login where nm_login = (SELECT nm_email from tb_cliente WHERE id_cliente = id);
+  delete from tb_cliente where id_cliente = id;
 end $$
 DELIMITER ;
