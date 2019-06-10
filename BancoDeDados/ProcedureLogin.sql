5fdca045512ce20f3562508c37278d401dc9512d
Loraine Rodrigues <loraine.rodrigues20@gmail.com>
2019-06-09 22:36:12 -0300

    Adição de procedure de adicionar admin

--- BancoDeDados/ProcedureLogin.sql
+++ BancoDeDados/ProcedureLogin.sql
@@ -6,10 +6,21 @@ DROP procedure IF EXISTS `verificaLogin`;
 
 DELIMITER $$
 
-USE `motofrete`$$
 CREATE PROCEDURE `verificaLogin` (IN email varchar(45), IN senha varchar(32))
 BEGIN
-	SELECT id_login, nm_usuario, id_tipo_login from tb_login where nm_usuario = email and id_senha = md5(senha);
+	SELECT id_login, nm_usuario, nm_login, id_tipo_login from tb_login where nm_login = email and id_senha = md5(senha);
+END$$
+
+DELIMITER ;
+
+DROP procedure IF EXISTS `cadastrarAdmin`;
+
+DELIMITER $$
+
+CREATE PROCEDURE `cadastrarAdmin` (IN nome VARCHAR(100), IN usuario varchar(45), IN senha varchar(32))
+BEGIN
+	INSERT INTO tb_login (nm_usuario, nm_login, id_senha, id_tipo_login)
+	VALUES (nome, usuario, md5(senha), '3');
 END$$
 
 DELIMITER ;
