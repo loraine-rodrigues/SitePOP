55bf43ad93baa31c34d054f8c41a78e61fefed20
Thereza Loraine <49696790+loraine-rodrigues@users.noreply.github.com>
2019-06-25 09:04:35 -0300

    Update comoFunciona.php

--- comoFunciona.php
+++ comoFunciona.php
@@ -1,136 +1,146 @@
 <?php
 $title = "COMO FUNCIONA";
-require 'conexao.php';
 include 'header.php';
 ?>
-<!-- Projects section v.3 -->
-<section class="my-5">
-<h1 class="h1-responsive text-center my-5">COMO FUNCIONA?</h1>
-<hr>
-  <!-- Section heading -->
-  <h2 class="h1-responsive font-weight-bold text-center my-5">Clientes</h2>
-  <!-- Section description -->
-  <p class="grey-text text-center w-responsive mx-auto mb-5">Veja como é rápido e fácil utilizar nossa plataforma</p>
 
-  <!-- Grid row -->
-  <div class="row m-3">
+<style type="text/css">
+    body {
+        width: 100%;
+        background-image: url(image/bg-contato.png) !important;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+</style>
 
-    <!-- Grid column -->
-    <div class="col-lg-5 mb-lg-0 mb-5">
-      <!--Image-->
-      <img src="image/cli-ft.png" alt="imagem cliente" class="img-fluid rounded z-depth-1">
+<div class="container text-center">
+    <h1>COMO FUNCIONA?</h1>
+    <div class="card mx-auto my-5" style="width: 56rem;">
+        <div class="card-body">
+
+            <h2 class="text-left ml-3 mt-3">Para Clientes</h2>
+            <p class="text-muted text-left ml-3 mb-3">Veja como é rápido e fácil utilizar nossa plataforma</p>
+
+            <hr style="width: 80%;" class="my-4" />
+
+            <div class="row">
+
+                <div class="col">
+                    <img alt="Imagem cliente" class="img-fluid rounded" src="image/clienteComoFunciona.png"/>
+                </div>
+
+                <div class="col">
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="fa fa-id-card fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Efetue seu cadastro</h3>
+                            <p class="grey-text text-justify">Após o cadastro, você terá total acesso a plataforma e aos Motofretistas disponíveis.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="fa fa-user-circle fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Escolha um Motofretista</h3>
+                            <p class="grey-text text-justify">No Catálogo você poderá filtrar e escolher o motofretista mais próximo ou que atenda suas necessidades.Você terá acesso aos dados profissionais e do veículo para sua maior segurança.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="fa fa-check fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Entre em contato</h3>
+                            <p class="grey-text text-justify">Negocie de forma rápida através do contato fornecido pelo motofretista na página de catálogo.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <hr style="width: 100%; color: black; height: 1px; background-color:black;"/>
+
+        <div class="card-body">
+
+            <h2 class="text-left ml-3 mt-3">Para Motofretistas</h2>
+            <p class="text-muted text-left ml-3 mb-3">Veja como é simples e fácil divulgar seu perfil</p>
+
+            <hr style="width: 80%;" class="my-4" />
+
+            <div class="row">
+
+                <div class="col">
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="fa fa-id-card fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Efetue seu cadastro</h3>
+                            <p class="grey-text text-justify">Ao realizá-lo você poderá divulgar seus serviços através de seu perfil visível para os clientes.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="fa fa-user-circle fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Perfil</h3>
+                            <p class="grey-text text-justify">Tenha acesso ao seu perfil com suas informações, podendo editá-las a qualquer momento.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-2 text-right">
+                            <i class="far fa-grin fa-2x"></i>
+                        </div>
+
+                        <div class="col-10 text-left">
+                            <h3>Aguarde contato</h3>
+                            <p class="grey-text text-justify">Aguarde até que o cliente que deseja contratar seus serviços com base no seus dados e perfil entre em contato com você através do número de WhatsApp fornecido.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <img alt="Imagem motofretista" class="img-fluid rounded m-5" src="image/motofretistaComoFunciona.png"/>
+                </div>
+
+            </div>
+
+        </div>
     </div>
-    <!-- Grid column -->
+</div>
 
-    <!-- Grid column -->
-    <div class="col-lg-7">
-
-      <!-- Grid row -->
-      <div class="row mb-3">
-        <div class="col-md-1 col-2">
-          <i class="fa fa-id-card fa-2x cyan-text"></i>
-        </div>
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Efetue seu cadastro</h5>
-          <p class="grey-text">Após o cadastro, você terá total acesso a plataforma e aos Motofretistas disponíveis.</p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-      <!-- Grid row -->
-      <div class="row mb-3">
-        <div class="col-md-1 col-2">
-          <i class="fa fa-user-circle fa-2x red-text"></i>
-        </div>
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Escolha um Motofretista</h5>
-          <p class="grey-text">No Catálogo você poderá filtrar e escolher o motofretista mais próximo ou que atenda suas necessidades.Você terá acesso aos
-          dados profissionais e do veículo para sua maior segurança.</p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-      <!-- Grid row -->
-      <div class="row">
-        <div class="col-md-1 col-2">
-          <i class="fa fa-check fa-2x deep-purple-text"></i>
-        </div>
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Entre em contato</h5>
-          <p class="grey-text mb-0">Negocie de forma rápida através do contato fornecido pelo motofretista no “catálogo”.</p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Grid row -->
-  <h2 class="h1-responsive font-weight-bold text-center my-5">Motofretistas</h2>
-  <hr class="my-5">
-
-  <!-- Grid row -->
-  <div class="row m-2">
-
-    <!-- Grid column -->
-    <div class="col-lg-7">
-
-      <!-- Grid row -->
-      <div class="row mb-3">
-        <div class="col-md-1 col-2">
-          <i class="fa fa-id-card fa-2x indigo-text"></i>
-        </div>
-        
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Efetue seu cadastro</h5>
-          <p class="grey-text">Ao realizá-lo você poderá divulgar seus serviços através de seu perfil visível para os clientes. 
-            </p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-      <!-- Grid row -->
-      <div class="row mb-3">
-        <div class="col-md-1 col-2">
-          <i class="fa fa-user-circle fa-2x pink-text"></i>
-        </div>
-        
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Perfil</h5>
-          <p class="grey-text">Tenha acesso ao seu perfil com suas informações, podendo editá-las a qualquer momento.</p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-      <!-- Grid row -->
-      <div class="row mb-lg-0 mb-5">
-        <div class="col-md-1 col-2">
-          <i class="far fa-grin fa-2x blue-text"></i>
-        </div>
-        <div class="col-md-11 col-10">
-          <h5 class="font-weight-bold mb-3">Aguarde contato</h5>
-          <p class="grey-text mb-0">Aguarde até que o cliente que deseja contratar seus serviços com base no seus dados e perfil entre em contato com você através do número de WhatsApp fornecido.</p>
-        </div>
-      </div>
-      <!-- Grid row -->
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-lg-4 m-auto">
-      <!--Image-->
-      <img src="image/ft-mt.png" alt="imagem motofretista" class="img-fluid rounded z-depth-1">
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Grid row -->
-
-</section>
-<!-- Projects section v.3 -->
-  <?php
-  include 'footer.php';
-  
-  
+<?php
+include 'footer.php';
+?>
